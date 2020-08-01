2874e222ca54344b53f4d0e5388545403922e66f
jiayounokim <kimjiwongit@gmail.com>
2020-08-01 11:39:34 +0530

    SHG App

--- reReddit_backend/settings.py
+++ reReddit_backend/settings.py
@@ -122,7 +122,6 @@ WSGI_APPLICATION = 'reReddit_backend.wsgi.application'
 #     }
 # }
 
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
@@ -177,6 +176,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
